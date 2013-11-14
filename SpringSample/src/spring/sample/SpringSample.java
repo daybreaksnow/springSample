9f30f079820fc2886fb822c629d51a6e3485265e
Toshiyuki Imaizumi <daybreaksnow@gmail.com>
2013-11-14 19:07:53 +0900

    引数で読み込むxmlを変えられるように

--- SpringSample/src/spring/sample/SpringSample.java
+++ SpringSample/src/spring/sample/SpringSample.java
@@ -5,8 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringSample {
 	public static void main(String[] args) {
+		String path = "spring-conf.xml";
+		if(args.length == 1){
+			path = args[0];
+			System.out.println("path=" + path);
+		}
 		ApplicationContext applicationContext = null;
-		applicationContext = new ClassPathXmlApplicationContext( "spring-conf.xml");
+		applicationContext = new ClassPathXmlApplicationContext( path);
 		Object bean = applicationContext.getBean("sampleBean");
 		Object subBeanShare = applicationContext.getBean("sampleSubBeanShare");
 		System.out.println(bean);
